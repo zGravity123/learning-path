ffec6c3bd9992d6c0a7ef9b214aa253f3c4625b8
zGravityy <fashoff777@gmail.com>
2026-04-21 10:29:25 +0100

    Update Ex26.c

--- C/Ex26.c
+++ C/Ex26.c
@@ -7,27 +7,23 @@ main()
 {
     setlocale(LC_ALL, "Portuguese");
 
-    int mes;
+    int dia;
 
-    printf("\033[36mIntroduza o mes em número \n");
-    scanf("%d", &mes);
+    printf("\033[36mIntroduza o dia em nÃºmero \n");
+    scanf("%d", &dia);
 
     system("cls");
 
-    switch(mes)
+    switch(dia)
     {
-        case 1: printf("Janeiro\n"); break;
-        case 2: printf("Fevereiro\n"); break;
-        case 3: printf("Março\n"); break;
-        case 4: printf("Abril\n"); break;
-        case 5: printf("Maio\n"); break;
-        case 6: printf("Junho\n"); break;
-        case 7: printf("Julho\n"); break;
-        case 8: printf("Agosto\n"); break;
-        case 9: printf("Setembro\n"); break;
-        case 10: printf("Outubro\n"); break;
-        case 11: printf("Novembro\n"); break;
-        case 12: printf("Dezembro\n"); break;
-        default: printf("\033[31m Mês Invalido! \033[0m \n");
+        case 1: printf("Domingo\n"); break;
+        case 2: printf("Segunda-Feira"); break;
+        case 3: printf("TerÃ§a-Feira\n"); break;
+        case 4: printf("Quarta-Feira\n"); break;
+        case 5: printf("Quinta-Feira\n"); break;
+        case 6: printf("Sexta-Feira\n"); break;
+        case 7: printf("SÃ¡bado\n"); break;
+
+        default: printf("\033[31m Dia Invalido! \033[0m \n");
     }
 }
