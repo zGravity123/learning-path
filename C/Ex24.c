bd3f2b8b75633d713a74dd36fe7752a20a25ef00
zGravityy <fashoff777@gmail.com>
2026-04-21 09:15:59 +0100

    Add files via upload

---
+++ C/Ex24.c
@@ -0,0 +1,10 @@
+// pedimos o valor em segundos e dizemos em minuto e os segundos que faltam por ex
+
+#include <stdio.h>
+#include <locale.h>
+
+main()
+{
+    set_locale(LC_ALL, "Portuguese");
+
+}
